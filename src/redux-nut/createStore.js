3b1cb4f1d3b713feee743b75ffe34a437d75d2b4
phm <506129591@qq.com>
2022-08-31 16:23:21 +0800

    first commit

---
+++ src/redux-nut/createStore.js
@@ -0,0 +1,33 @@
+export default function createStore(reducer, enhancer) {
+  if (enhancer) {
+    return enhancer(createStore)(reducer);
+  }
+  let currentState;
+  let currentListeners = [];
+
+  function getState() {
+    return currentState;
+  }
+
+  function dispatch(action) {
+    currentState = reducer(currentState, action);
+    currentListeners.forEach((listener) => listener());
+  }
+
+  function subscribe(listener) {
+    currentListeners.push(listener);
+
+    return () => {
+      const index = currentListeners.indexOf(listener);
+      currentListeners.splice(index, 1);
+    };
+  }
+
+  dispatch({ type: "ASASASASASA/REDUX" }); // currentState初始值赋值
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+}
---
+++ src/store/index.js
@@ -0,0 +1,63 @@
+// import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "../redux-nut";
+
+// import thunk from "redux-thunk";
+// import logger from "redux-logger";
+import promise from "redux-promise";
+
+// 定义了store修改规则
+export function countReducer(state = 0, action) {
+  switch (action.type) {
+    case "ADD":
+      return state + 1;
+    case "MINUS":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// 创建store
+const store = createStore(
+  // countReducer,
+  combineReducers({
+    count: countReducer,
+  }),
+  applyMiddleware(
+    promise,
+    thunk,
+    logger
+  )
+);
+
+export default store;
+function logger({ getState, dispatch }) {
+  return (next) => (action) => {
+    console.log("-----------------------"); //sy-log
+
+    console.log(action.type + "执行了"); //sy-log
+
+    const prevState = getState();
+
+    console.log("prev state", prevState); //sy-log
+
+    const returnValue = next(action);
+
+    // 等状态值修改之后，再执行getState，拿到了新的状态值
+    const nextState = getState();
+
+    console.log("next state", nextState); //sy-log
+
+    console.log("-----------------------"); //sy-log
+    console.log(returnValue)
+    return returnValue;
+  };
+}
+function thunk({ getState, dispatch }) {
+  return (next) => (action) => {
+    if (typeof action === "function") {
+      return action(dispatch, getState);
+    }
+    return next(action);
+  };
+}
